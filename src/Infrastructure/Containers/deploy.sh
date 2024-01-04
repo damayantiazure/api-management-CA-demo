71ecb7d126094476c40444dedadca84a742539c7
damayantiazure <92169356+damayantiazure@users.noreply.github.com>
2024-01-04 10:13:59 +0100

    Update deploy.sh

--- src/Infrastructure/Containers/deploy.sh
+++ src/Infrastructure/Containers/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+//var appname = 'containerapp-demo-api'
+//var appEnv = 'Containerapp-demo-env'
 
 export resourceGroupName="APIM-DEVOPS"
 export location="westeurope"
-export APP_NAME="solar"
-export APP_ENV="dev"
+export APP_NAME="containerapp-demo-api"
+export APP_ENV="Containerapp-demo-env"
 export TAG_NAME="beta2"
 
 echo "Starting deploying the app provisioning..."
